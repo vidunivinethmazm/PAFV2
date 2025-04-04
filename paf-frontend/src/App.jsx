fa0bbb54fa4ffca30d4b1439577b3f7a4de4e442
Viduni <it22170316@my.sliit.lk>
2025-04-05 00:05:55 +0530

    setup frontend

---
+++ paf-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        hello
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
