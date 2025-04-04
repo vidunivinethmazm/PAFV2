de335faedd5c7a5860f49177db03df2e5d8bbc47
Viduni <it22170316@my.sliit.lk>
2025-04-05 00:04:30 +0530

    initial commit

---
+++ paf-backend-2025/src/main/java/com/example/pafbackend/PafBackend2025Application.java
@@ -0,0 +1,13 @@
+package com.example.pafbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafBackend2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PafBackend2025Application.class, args);
+    }
+
+}
---
+++ paf-backend-2025/src/test/java/com/example/pafbackend/PafBackend2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pafbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafBackend2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
