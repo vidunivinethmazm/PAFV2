cbd604a2100c193c77e655b7cbe9bac600b651e6
it22117250 <it22117250@my.sliit.lk>
2025-04-06 12:10:00 +0530

    Like DTO

---
+++ paf-backend-2025/src/main/java/com/example/pafbackend/dtos/LikeDTO.java
@@ -0,0 +1,23 @@
+package com.example.pafbackend.dtos;
+
+public class LikeDTO {
+    private String userId;
+    private String postId;
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+}
