9dbef3509fb139d581dfc496cebfb11624612eb0
it22117250 <it22117250@my.sliit.lk>
2025-04-08 14:16:00 +0530

    Media Type Response DTO

---
+++ paf-backend-2025/src/main/java/com/example/pafbackend/dtos/MediaTypeResponseDTO.java
@@ -0,0 +1,55 @@
+package com.example.pafbackend.dtos;
+
+import com.example.pafbackend.models.Post;
+import java.util.Date;
+
+public class MediaTypeResponseDTO {
+    private String id;
+    private String type;
+    private String url;
+    private Date createdAt;
+    private String postId;
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
