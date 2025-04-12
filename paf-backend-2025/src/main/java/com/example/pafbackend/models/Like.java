802092ce2b254ff5c1dc1762ea9879f03f1c6ae5
it22117250 <it22117250@my.sliit.lk>
2025-04-12 12:00:00 +0530

    Like Model

---
+++ paf-backend-2025/src/main/java/com/example/pafbackend/models/Like.java
@@ -0,0 +1,56 @@
+package com.example.pafbackend.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+@Document(collection = "likes")
+public class Like {
+    @Id
+    private String id;
+    private Date createdAt;
+
+    @DBRef
+    private AppUser user;
+
+    @DBRef
+    private Post post;
+
+    public Like() {
+        this.createdAt = new Date();
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public AppUser getUser() {
+        return user;
+    }
+
+    public void setUser(AppUser user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
