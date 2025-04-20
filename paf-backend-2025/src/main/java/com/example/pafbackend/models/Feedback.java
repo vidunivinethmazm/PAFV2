01f5bfa5a3717e5832731a7cf1156cb3fde41efa
Viduni <it22170316@my.sliit.lk>
2025-04-20 20:50:16 +0530

    create feedback model

---
+++ paf-backend-2025/src/main/java/com/example/pafbackend/models/Feedback.java
@@ -0,0 +1,77 @@
+package com.example.pafbackend.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+@Document(collection = "feedbacks")
+public class Feedback {
+    @Id
+    private String id;
+    private String feedback;
+    private int numOfStars;
+    private boolean deleteStatus;
+    private Date createdAt;
+
+    @DBRef
+    private AppUser createdBy;
+
+
+
+    public Feedback() {
+        this.createdAt = new Date();
+        this.deleteStatus = false;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public int getNumOfStars() {
+        return numOfStars;
+    }
+
+    public void setNumOfStars(int numOfStars) {
+        this.numOfStars = numOfStars;
+    }
+
+    public boolean isDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public void setDeleteStatus(boolean deleteStatus) {
+        this.deleteStatus = deleteStatus;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public AppUser getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(AppUser createdBy) {
+        this.createdBy = createdBy;
+    }
+
+
+}
